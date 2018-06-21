98c5772792799f56f3d3490684059d7205e2c9ef
user-penguin <kobzev96@gmail.com>
2018-06-22 02:51:32 +0700

    a small changes

--- src/main/java/Lab_4.java
+++ src/main/java/Lab_4.java
@@ -1,11 +1,20 @@
 /**
  * частичная проблема собственных чисел
  */
+
 public class Lab_4 {
+
+    private double personalNumbers;
+    private double[] personalVector;
+    private double[] firstApproximation;
 
     public Lab_4(String path) {
         Matrix matrix = new Matrix();
         matrix.fillFromFile(path);
+
+        firstApproximation = MatrixMath.getRandomVector(matrix.getMatrix().length);
+        personalVector = MatrixMath.searchPersonalVector(matrix.getMatrix(), firstApproximation);
+        personalNumbers = MatrixMath.searchPersonalNumber(matrix.getMatrix(), personalVector);
 
 
 
