b41ceb4ed625c5071432628260def0850e6debd0
FedorovVladimir <vladimir.fodorow@gmail.com>
2018-06-22 03:43:55 +0700

    clear Matrix

--- src/main/java/Matrix.java
+++ src/main/java/Matrix.java
@@ -1,25 +1,29 @@
 import java.io.*;
 import java.util.ArrayList;
 
-import static java.lang.Math.sqrt;
-
 public class Matrix {
 
     protected double[][] matrix;
 
     public Matrix() {}
 
+    public Matrix(int width, int height) {
+        matrix = new double[height][width];
+    }
+
     public Matrix(double[][] matrix) {
         this.matrix = matrix;
     }
 
-    private void addOneRow(String read, ArrayList<double[]> matrix) {
-        String[] toSplit = read.split(" ");
-        int width = toSplit.length;
-        double oneString[] = new double[width];
-        for(int i = 0; i < width; i++)
-            oneString[i] = Double.parseDouble(toSplit[i]);
-        matrix.add(oneString);
+    public double[][] getMatrix() {
+        return matrix;
+    }
+
+    public double[][] toArray(ArrayList<double[]> matrixArr) {
+        double[][] matrix = new double[matrixArr.size()][matrixArr.get(0).length];
+        for(int i = 0; i < matrixArr.size(); i++)
+            System.arraycopy (matrixArr, 0, matrix, 0, matrix.length);
+        return matrix;
     }
 
     public void fillFromFile(String path) {
@@ -56,39 +60,6 @@ public class Matrix {
         }
     }
 
-    public double[][] toArray(ArrayList<double[]> matrixArr) {
-        double[][] matrix = new double[matrixArr.size()][matrixArr.get(0).length];
-        for(int i = 0; i < matrixArr.size(); i++)
-            System.arraycopy (matrixArr, 0, matrix, 0, matrix.length);
-        return matrix;
-    }
-
-    public double[][] getMatrix() {
-        return matrix;
-    }
-
-    public double calculateAlpha(int k, int l) {
-        if (matrix[k][k] == matrix[l][l])
-            return Math.sqrt(0.5);
-        else {
-            double mu = calculateMu(k, l);
-            return Math.sqrt((1 + 1 / (Math.sqrt(1 + mu * mu))) / 2);
-        }
-    }
-
-    public double calculateBetta(int k, int l) {
-        if (matrix[k][k] == matrix[l][l])
-            return Math.sqrt(0.5);
-        else {
-            double mu = calculateMu(k, l);
-            return Math.signum(mu) * Math.sqrt((1 - 1 / (Math.sqrt(1 + mu * mu))) / 2);
-        }
-    }
-
-    private double calculateMu(int k, int l){
-        return (2 * matrix[k][l]) / (matrix[k][k] - matrix[l][l]);
-    }
-
     public void rotation() {
         int size = matrix.length;
 
@@ -100,7 +71,7 @@ public class Matrix {
 
         double[] lambda = new double[size];
         int[] indexes = new int[2];
-        while (Math.abs(getElement(sumRowMatrix, indexes)) < 0) {
+        while (Math.abs(searchElement(sumRowMatrix, indexes)) < 0) {
             // todo
         }
 
@@ -108,7 +79,16 @@ public class Matrix {
             lambda[i] = matrix[i][i];
     }
 
-    private double getElement(double[] sumRowMatrix, int[] indexes) {
+    private void addOneRow(String read, ArrayList<double[]> matrix) {
+        String[] toSplit = read.split(" ");
+        int width = toSplit.length;
+        double oneString[] = new double[width];
+        for(int i = 0; i < width; i++)
+            oneString[i] = Double.parseDouble(toSplit[i]);
+        matrix.add(oneString);
+    }
+
+    private double searchElement(double[] sumRowMatrix, int[] indexes) {
         double max = 0;
         indexes[0] = 0;
         for (int i = 0; i < matrix.length; i++)
@@ -132,4 +112,27 @@ public class Matrix {
         }
         return max;
     }
+
+    private double calculateAlpha(int k, int l) {
+        if (matrix[k][k] == matrix[l][l])
+            return Math.sqrt(0.5);
+        else {
+            double mu = calculateMu(k, l);
+            return Math.sqrt((1 + 1 / (Math.sqrt(1 + mu * mu))) / 2);
+        }
+    }
+
+    private double calculateBetta(int k, int l) {
+        if (matrix[k][k] == matrix[l][l])
+            return Math.sqrt(0.5);
+        else {
+            double mu = calculateMu(k, l);
+            return Math.signum(mu) * Math.sqrt((1 - 1 / (Math.sqrt(1 + mu * mu))) / 2);
+        }
+    }
+
+    private double calculateMu(int k, int l){
+        return (2 * matrix[k][l]) / (matrix[k][k] - matrix[l][l]);
+    }
+
 }
