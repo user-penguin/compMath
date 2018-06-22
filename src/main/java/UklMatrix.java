608caae4aaf3092f3811443f7a037f85229b71be
FedorovVladimir <vladimir.fodorow@gmail.com>
2018-06-22 17:57:11 +0700

    update UklMatrix

--- src/main/java/UklMatrix.java
+++ src/main/java/UklMatrix.java
@@ -2,15 +2,15 @@ public class UklMatrix extends Matrix {
 
     // constructors
 
-    UklMatrix(int size, int k, int l, double alpha, double betta) {
+    UklMatrix(int size, int k, int l) {
         matrix = new double[size][size];
 
         for(int i = 0; i < size; i++)
             matrix[i][i] = 1;
 
-        matrix[k][k] = matrix[l][l] = alpha;
-        matrix[k][l] = -1 * betta;
-        matrix[l][k] = betta;
+        matrix[k][k] = matrix[l][l] = calculateAlpha(k, l);
+        matrix[k][l] = -1 * calculateBetta(k, l);
+        matrix[l][k] = calculateBetta(k, l);
     }
 
 }
