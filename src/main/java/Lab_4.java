8c49999f4f5880f7c2a773254c7fdf1d88fa211b
user-penguin <kobzev96@gmail.com>
2018-06-22 17:31:20 +0700

    create 4lab

--- src/main/java/Lab_4.java
+++ src/main/java/Lab_4.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * частичная проблема собственных чисел
  */
@@ -15,8 +17,29 @@ public class Lab_4 {
         personalVector = MatrixMath.searchPersonalVector(matrix.getMatrix(), firstApproximation);
         personalNumbers = MatrixMath.searchPersonalNumber(matrix.getMatrix(), personalVector);
 
+        System.out.println("Собственный вектор, соответствующий " +
+                "наибольшему по модулю собственному числу:");
+        new Matrix().print(personalVector);
+        System.out.println("Наибольшее по модулю собственное число = " + personalNumbers);
+
+        double[] secondPersonalVector = MatrixMath.searchSecPersVector(matrix.getMatrix(),
+                personalVector, firstApproximation);
+        double secondPersonalNumber = MatrixMath.searchPersonalNumber(matrix.getMatrix(), secondPersonalVector);
 
 
-        matrix.print();
+        System.out.println("Собственный вектор, соответствующий " +
+                "второму по абсолютной величине собственному числу:");
+        new Matrix().print(secondPersonalVector);
+        System.out.println("Второе по абсолютной величине собственное число = " + secondPersonalNumber);
+
+
+        System.out.println("Введите число, к которому будем искать ближайшее собственное");
+        Scanner in = new Scanner(System.in);
+
+
+        double nearNumber = in.nextDouble();
+        System.out.println("Ближайшее к " + nearNumber + "= " +
+                MatrixMath.searchCloserNumber(nearNumber, matrix.getMatrix()));
+
     }
 }
