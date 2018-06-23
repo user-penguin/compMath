0d09ae5cc92187e6c3d25d8587e9d47e1b724876
FedorovVladimir <vladimir.fodorow@gmail.com>
2018-06-24 01:26:16 +0700

    Update Lab_7

--- src/main/java/Lab_7.java
+++ src/main/java/Lab_7.java
@@ -1,17 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
- * ИНТЕРПОЛИРОВАНИЕ
+ * Интеpполиpование сплайнами
  */
 
 public class Lab_7 {
 
     public static void main(String[] arg) {
-        Lab_6 lab_6 = new Lab_6("input.txt");
+        Lab_7 lab_7 = new Lab_7("input.txt");
     }
 
     // constructors
 
     public Lab_7(String path) {
-
+        Graphic graphic = new Graphic();
+        graphic.addLine(new int[]{0, 20, 30}, new int[]{0, 20, 70});
+        graphic.paint();
     }
 
 }
