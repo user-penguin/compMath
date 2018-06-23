a4a7ddd3b6109b5965bcc0b1c06519f792e4a0f3
FedorovVladimir <vladimir.fodorow@gmail.com>
2018-06-24 01:23:31 +0700

    Add Graphic

--- src/main/java/Graphic.java
+++ src/main/java/Graphic.java
@@ -1,36 +1,75 @@
-package lr6;
-
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import javax.swing.JComponent;
+import java.awt.*;
+import java.util.ArrayList;
+import javax.swing.*;
 
 public class Graphic extends JComponent {
-    int x1[] = new int[101];
-    int y1[] = new int[101];
-    int x2[] = new int[101];
-    int y2[] = new int[101];
+
+    private JFrame frame = new JFrame();
+
+    private int countLines;
+
+    private int x[][] = new int[100][];
+    private int y[][] = new int[100][];
+
+    public Graphic() {
+        frame.setTitle("Графики функций");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 700);
+        frame.setBackground(Color.WHITE);
+        frame.setLocationRelativeTo(null);
+        frame.add(this);
+        frame.setVisible(true);
+    }
+
+    public void paint() {
+        paint(this.getGraphics());
+    }
+
+    @Override
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D)g;
-        g2d.drawLine(50, 300, 800, 300);
-        g2d.drawLine(800, 300, 785, 290);
-        g2d.drawLine(800, 300, 785, 310);
-        g2d.drawLine(400, 50, 400, 600);
-        g2d.drawLine(400, 50, 390, 65);
-        g2d.drawLine(400, 50, 410, 65);
-        g2d.setColor(Color.blue);
-        g2d.drawPolyline(x1, y1, 101);
-        g2d.setColor(Color.RED);
-        g2d.drawPolyline(x2, y2, 101);
-        //g2d.setColor(Color.blue);
-        //g2d.drawPolyline(x1, y1, 101);
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON); //сглаживание
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+        paintXY(g2d);
+        paintLines(g2d);
     }
-    public void graph(Graphics g, int xI1[], int yI1[], int xI2[], int yI2[]) {
-        x1 = xI1;
-        y1 = yI1;
-        x2 = xI2;
-        y2 = yI2;
-        paint(g);
+
+    private void paintXY(Graphics2D g2d) {
+        int padding = 30;
+        g2d.drawLine(padding, getHeight() / 2, getWidth() - padding, getHeight() / 2);
+        g2d.drawLine(getWidth() - padding, getHeight() / 2, getWidth() - padding - 15, getHeight() / 2 - 10);
+        g2d.drawLine(getWidth() - padding, getHeight() / 2, getWidth() - padding - 15, getHeight() / 2 + 10);
+        g2d.drawLine(getWidth() / 2, padding, getWidth() / 2, getHeight() - padding);
+        g2d.drawLine(getWidth() / 2, padding, getWidth() / 2 - 10, padding + 15);
+        g2d.drawLine(getWidth() / 2, padding, getWidth() / 2 + 10, padding + 15);
+    }
+
+    private void paintLines(Graphics2D g2d) {
+        for (int i = 0; i < countLines; i++) {
+            g2d.setStroke(new BasicStroke(3.0f));
+            if (i == 0)
+                g2d.setColor(Color.blue);
+            if (i == 1)
+                g2d.setColor(Color.red);
+            if (i == 2)
+                g2d.setColor(Color.green);
+
+            int[] x = new int[this.x[i].length];
+            int[] y = new int[this.x[i].length];
+
+            for (int j = 0; j < x.length; j++){
+                x[j] = this.x[i][j] + getWidth() / 2;
+                y[j] = - this.y[i][j] + getHeight() / 2;
+            }
+            g2d.drawPolyline(x, y, x.length);
+        }
+    }
+
+    public void addLine(int[] x, int[] y) {
+        this.x[countLines] = x;
+        this.y[countLines] = y;
+        countLines++;
     }
 }
