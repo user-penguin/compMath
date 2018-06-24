a20cc3f7e57563fc2fc48b7074261da3dc6e2281
user-penguin <kobzev96@gmail.com>
2018-06-25 01:31:38 +0700

    main Lab6

--- src/main/java/Lab_6.java
+++ src/main/java/Lab_6.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 /**
  * ИНТЕРПОЛИРОВАНИЕ
  */
@@ -5,13 +8,35 @@
 public class Lab_6 {
 
     public static void main(String[] arg) {
-        Lab_6 lab_6 = new Lab_6("input.txt");
+        Lab_6 lab_6 = new Lab_6("src\\test\\source\\Lab6_in.txt");
     }
 
     // constructors
 
     public Lab_6(String path) {
+        Scanner in = new Scanner(System.in);
+        InterpolMethods IM = new InterpolMethods();
+        IM.fillFromFile(path);
 
+        String function;
+        System.out.println("Введите название функции, с которой будем работать: ");
+        function = in.next();
+        IM.setInterpolateValues(function);
+
+        IM.setTargetX();
+
+        System.out.println("Значение в точке " + IM.getTargetX() + " по Лагранжу: "
+                + IM.getLagrangePolyn(IM.getTargetX()));
+        System.out.println("Значение в точке " + IM.getTargetX() + " по Ньютону: "
+                + IM.getNewtonPolyn(IM.getTargetX()));
+
+        double[] arguments = IM.getGraphicArg(1000);
+        double[] newtonValues = IM.getNewtonValues(arguments);
+        double[] realValues = IM.getRealValues(arguments, function);
+
+        JFreeChartTools graphic = new JFreeChartTools(function, arguments, newtonValues, realValues);
+        graphic.pack();
+        graphic.setVisible(true);
     }
 
 }
