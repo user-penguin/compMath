430ba0f3f4c80f464c17f4eae15fac1b60255a90
user-penguin <kobzev96@gmail.com>
2018-06-25 01:29:44 +0700

    edit and restructuring methods

--- src/main/java/InterpolMethods.java
+++ src/main/java/InterpolMethods.java
@@ -1,9 +1,7 @@
 import jdk.internal.org.objectweb.asm.tree.analysis.Interpreter;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class InterpolMethods {
@@ -12,12 +10,26 @@ public class InterpolMethods {
     private double[] interpolateValues;
     private double targetX;
 
-    public double[] getNodes() {
+    public double[] getInterpolateNodes() {
         return interpolateNodes;
     }
 
     public double[] getInterpolateValues() {
         return interpolateValues;
+    }
+
+    public double getTargetX() {
+        return targetX;
+    }
+
+    public void setInterpolateValues(String function) {
+        interpolateValues = new double[interpolateNodes.length];
+        for (int i = 0; i < interpolateNodes.length; i++)
+            interpolateValues[i] = calcValueAvailFunction(function, interpolateNodes[i]);
+    }
+
+    public void setTargetX(double value) {
+        targetX = value;
     }
 
     public void setTargetX() {
@@ -37,13 +49,41 @@ public class InterpolMethods {
 
             read  = br.readLine();
             interpolateNodes = splitToDoubleArray(read);
-
-            read = br.readLine();
-            interpolateValues = splitToDoubleArray(read);
         }
         catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public double[] getGraphicArg(int size) {
+        double minArgument = interpolateNodes[0];
+        double maxArgument = interpolateNodes[interpolateNodes.length - 1];
+        double step = Math.abs(maxArgument - minArgument) / size;
+        double x = minArgument;
+        double[] steps = new double[size];
+
+        for (int i = 0; i < size; i++) {
+            steps[i] = x;
+            x += step;
+        }
+
+        return steps;
+    }
+
+    public double[] getNewtonValues(double[] arguments) {
+        double[] newtonValues = new double[arguments.length];
+        for (int i = 0; i < arguments.length; i++)
+            newtonValues[i] = getNewtonPolyn(arguments[i]);
+
+        return newtonValues;
+    }
+
+    public double[] getRealValues(double[] arguments, String function) {
+        double[] values = new double[arguments.length];
+        for (int i = 0; i < arguments.length; i++)
+            values[i] = calcValueAvailFunction(function, arguments[i]);
+
+        return  values;
     }
 
     private double[] splitToDoubleArray(String source) {
@@ -57,6 +97,45 @@ public class InterpolMethods {
             finalArray[i] = Double.parseDouble(splittingNumbers[i]);
 
         return finalArray;
+    }
+
+    public double calcValueAvailFunction(String function, double argument) {
+        switch (function) {
+            case "x^2":
+                return argument * argument;
+
+            case "sin(x)":
+                return Math.sin(argument);
+
+            case "cos(x)":
+                return Math.cos(argument);
+
+            case "x^3":
+                return argument * argument * argument;
+
+            case "|x|":
+                return Math.abs(argument);
+
+            case "10*x":
+                return argument * 10;
+
+            default:
+                throw new UnsupportedOperationException();
+        }
+    }
+
+    public double getDividedDiff(int n) {
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            double multip = 1;
+            for (int l = 0; l < n; l++) {
+                if (i != l) {
+                    multip *= interpolateNodes[i] - interpolateNodes[l];
+                }
+            }
+            sum += interpolateValues[i] / multip;
+        }
+        return sum;
     }
 
     public double getLagrangePolyn(double targetX){
@@ -73,28 +152,6 @@ public class InterpolMethods {
             sum += (multipUp * interpolateValues[k]) / multipDown;
         }
 
-        return sum;
-    }
-//
-//    public double calcValueAvailFunction(int number) {
-//        switch (number) {
-//            case 1:
-//                return targetX * targetX;
-//        }
-//    }
-
-
-    public double getDividedDiff(int n) {
-        double sum = 0;
-        for (int i = 0; i < n; i++) {
-            double multip = 1;
-            for (int l = 0; l < n; l++) {
-                if (i != l) {
-                    multip *= interpolateNodes[i] - interpolateNodes[l];
-                }
-            }
-            sum += interpolateValues[i] / multip;
-        }
         return sum;
     }
 
