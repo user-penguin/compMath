9f440c8c9cb664df03e88071872f63687bcf7369
user-penguin <kobzev96@gmail.com>
2018-06-25 01:30:44 +0700

    edit tests

--- src/test/java/InterpolMethodsTest.java
+++ src/test/java/InterpolMethodsTest.java
@@ -10,24 +10,31 @@ class InterpolMethodsTest {
         String path = "src\\test\\source\\testFillNodes.txt";
         InterpolMethods IM = new InterpolMethods();
         IM.fillFromFile(path);
-        double[] expected1 = IM.getNodes();
+        double[] expected1 = IM.getInterpolateNodes();
         double[] actual1 = new double[]{1, 2, 3, 4};
 
-        double[] expected2 = IM.getInterpolateValues();
-        double[] actual2 = new double[]{10, 20, 30, 40};
-
         assertArrayEquals(expected1, actual1);
-        assertArrayEquals(expected2, actual2);
     }
 
     @Test
     void getLagrangePolyn() {
-        String path = "D:\\Projects\\forTests\\testFillNodes.txt";
+        String path = "src\\test\\source\\testFillNodes.txt";
         InterpolMethods IM = new InterpolMethods();
         IM.fillFromFile(path);
+        IM.setInterpolateValues("10*x");
 
         double expected = IM.getLagrangePolyn(2.5);
         double actual = 25;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void calcValueAvailFunction() {
+        String funk = "x^2";
+        InterpolMethods IM = new InterpolMethods();
+        IM.setTargetX(6);
+        double expected = IM.calcValueAvailFunction(funk, IM.getTargetX());
+        double actual = 36;
         assertEquals(expected, actual);
     }
 }
