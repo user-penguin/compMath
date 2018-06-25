0a116893b5a2003fcdc47ec4ff19e50733e86f55
user-penguin <kobzev96@gmail.com>
2018-06-25 14:08:59 +0700

    code review for lab5

--- src/main/java/Lab_5.java
+++ src/main/java/Lab_5.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 import static java.lang.Math.abs;
 
 /**
@@ -6,43 +12,108 @@ import static java.lang.Math.abs;
  * https://lektsii.org/13-70062.html
  */
 
-public class Lab_5 {
+public class Lab_5 extends Lab {
+
+    private double[][] A;
+    private double[][] start;
+    private double[]personalNumbers;
+    private double[] sumA;
+    private UnitMatrix e; // мать собственных векторов
+    private Matrix nevjazki;
+
+
 
     public static void main(String[] arg) {
-        Lab_5 lab_5 = new Lab_5("input.txt");
+        Lab_5 lab_5 = new Lab_5();
+        lab_5.fillFromFile("src\\test\\source\\file_lab_5.txt");
+        lab_5.run();
+        lab_5.print();
     }
 
-    // constructors
+    private void addOneRow(String readRow, ArrayList<double[]> matrix) {
+        String[] toSplit = readRow.split(" ");
+        int width = toSplit.length;
+        double oneString[] = new double[width];
+        for(int i = 0; i < width; i++)
+            oneString[i] = Double.parseDouble(toSplit[i]);
+        matrix.add(oneString);
+    }
 
-    public Lab_5(String path) {
-        Matrix start = new Matrix(path);
-        Matrix A = new Matrix(path);
-        double[] sumA = calculateSumA(A.getMatrix());
-        UnitMatrix e = new UnitMatrix(A.getMatrix().length);
+    private double[][] toArray(ArrayList<double[]> matrixArr) {
+        double[][] matrix = new double[matrixArr.size()][matrixArr.get(0).length];
+        for(int i = 0; i < matrixArr.size(); i++)
+            for(int j = 0; j < matrixArr.size(); j++)
+                matrix[i][j] = matrixArr.get(i)[j];
+        return matrix;
+    }
+
+    @Override
+    public void fillFromFile(String path) {
+        ArrayList<double[]> matrix = new ArrayList<>();
+        try {
+            FileInputStream fstream = new FileInputStream(path);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            String read;
+
+            while((read = br.readLine()) != null)
+                addOneRow(read, matrix);
+            this.A = toArray(matrix);
+            this.start = new double[this.A.length][this.A.length];
+            for (int i = 0; i < this.A.length; i++)
+                for (int j = 0; j < this.A.length; j++)
+                    this.start[i][j] = this.A[i][j];
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        sumA = calculateSumA(this.A);
+        e = new UnitMatrix(A.length);
         int[] indexes = new int[2];
 
-        while (!(searchMaxElem(A.getMatrix(), indexes, sumA) < MatrixMath.E)) {
-            Matrix U = new Matrix(uklMatrix(A.getMatrix(), indexes[0], indexes[1]));
+        while (!(searchMaxElem(A, indexes, sumA) < MatrixMath.E)) {
+            Matrix U = new Matrix(uklMatrix(A, indexes[0], indexes[1]));
 
             // A = UT * A * U
-            A = new Matrix(MatrixMath.multip(U.transpose(), A.getMatrix()));
-            A = new Matrix(MatrixMath.multip(A.getMatrix(), U.getMatrix()));
 
+            A = new Matrix(MatrixMath.multip(U.transpose(), A)).getMatrix();
+            A = new Matrix(MatrixMath.multip(A, U.getMatrix())).getMatrix();
+
+            // e = e * U
             e = new UnitMatrix(MatrixMath.multip(e.getMatrix(), U.getMatrix()));
 
-            sumA = calculateSumA(A.getMatrix());
+            sumA = calculateSumA(A);
         }
 
-        double[] personalNumbers = new double[A.getMatrix().length];
+        personalNumbers = new double[A.length];
         for (int i = 0; i < personalNumbers.length; i++)
-            personalNumbers[i] = A.getMatrix()[i][i];
+            personalNumbers[i] = A[i][i];
 
         normalization(e.getMatrix());
 
-        Matrix nevjazki = new Matrix(new double[A.getMatrix().length][A.getMatrix().length]);
-        nevjazka(start.getMatrix(), personalNumbers, e.getMatrix(), nevjazki.getMatrix());
+        nevjazki = new Matrix(new double[A.length][A.length]);
+        nevjazka(this.start, personalNumbers, e.getMatrix(), nevjazki.getMatrix());
 
-        print(personalNumbers, e, nevjazki, start);
+    }
+
+    @Override
+    public void print() {
+        Matrix startMatrix = new Matrix(this.start);
+        startMatrix.print("Исходная матрица A: ");
+        for (int i = 0; i < personalNumbers.length; i++) {
+            System.out.println("\nСобственное число №" + (i + 1) + ": " + personalNumbers[i]);
+            System.out.print("Собственный вектор:    (");
+            for (int j = 0; j < personalNumbers.length; j++)
+                System.out.print(e.getMatrix()[j][i] + " ");
+            System.out.println(")");
+            System.out.print("Невязка: ");
+            for (int j = 0; j < personalNumbers.length; j++)
+                System.out.print(nevjazki.getMatrix()[i][j] + " ");
+            System.out.println();
+        }
     }
 
     // private
