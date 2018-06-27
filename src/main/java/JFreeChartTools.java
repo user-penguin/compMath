40e88b78c22504a64aef9cc2618cda94700f1b5b
user-penguin <kobzev96@gmail.com>
2018-06-27 12:00:04 +0700

    fixed graphics

--- src/main/java/JFreeChartTools.java
+++ src/main/java/JFreeChartTools.java
@@ -29,4 +29,32 @@ public class JFreeChartTools extends ApplicationFrame {
         chartPanel.setPreferredSize(new java.awt.Dimension(500, 500));
         setContentPane(chartPanel);
     }
+
+    public JFreeChartTools(String title, double[] nodes, double[] splineVal, double[] polynVal, double[] realVal) {
+
+        super(title);
+
+        XYSeriesCollection dataset = new XYSeriesCollection();
+
+        XYSeries splineGraphSeries = new XYSeries("spline");
+        for (int i = 0; i < nodes.length; i++)
+            splineGraphSeries.add(nodes[i], splineVal[i]);
+        dataset.addSeries(splineGraphSeries);
+
+        XYSeries polynGraphSeries = new XYSeries("polyn");
+        for (int i = 0; i < nodes.length; i++)
+            polynGraphSeries.add(nodes[i], polynVal[i]);
+        dataset.addSeries(polynGraphSeries);
+
+        XYSeries realGraphSeries = new XYSeries("real");
+        for (int i = 0; i < nodes.length; i++)
+            realGraphSeries.add(nodes[i], realVal[i]);
+        dataset.addSeries(realGraphSeries);
+
+        JFreeChart chart = ChartFactory.createScatterPlot("compare : " + title,
+                "X", "Y", dataset, PlotOrientation.VERTICAL,true,true,false);
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(500, 500));
+        setContentPane(chartPanel);
+    }
 }
